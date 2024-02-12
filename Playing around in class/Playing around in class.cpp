8ef18cdf8afdcad5181f02b95cc581abbcdfa069
RJay02 <115995680+RJay02@users.noreply.github.com>
2024-02-12 20:54:25 +0000

    creating the parameterised constructor

--- Playing around in class/Playing around in class.cpp
+++ Playing around in class/Playing around in class.cpp
@@ -6,34 +6,51 @@ using namespace std;
 
 //Creating the Animal class
 class Animal {
-	string Species; 
+public:
+	string Species;
 	string Name;
-	string Colour; 
-	string Age; 
-	string Weight; 
-	string Noise; 
+	string Colour;
+	int Age;
+	int Weight;
+	string Noise;
+
+
+	Animal(string SpeciesVal, string NameVal, string ColourVal, int AgeVal, int WeightVal, string NoiseVal) {
+		Species = SpeciesVal;
+		Name = NameVal;
+		Colour = ColourVal;
+		Age = AgeVal;
+		Weight = WeightVal;
+		Noise = NoiseVal;
+	}
 
 	void sayHello() {
-		cout << "The animal's name is" << Name << endl; 
+		cout << "The animal's name is" << Name << endl;
 	}
 
 	void makeNoise() {
-		cout << "The animal goes" << Noise << endl; 
+		cout << "The animal goes" << Noise << endl;
 	}
 
 	void eat(string food) {
-		cout << "Yummy, the animal likes" << food << endl; 
+		cout << "Yummy, the animal likes" << food << endl;
 	}
 
 	void animal() {
-		cout << "Animal has been created" << endl; 
+		cout << "Animal has been created" << endl;
 	}
-}
+
+
+};
 
 
 
+int main(){
+	Animal Cat("British Short hair", "Milo", "Ginger", "2", "10", "meowwww");
 
-int main()
-{
-   cout << "Hello World!\n";
+	Cat.sayHello(); 
+	Cat.makeNoise(); 
+	Cat.eat("fish");
+
+	return 0; 
 }
