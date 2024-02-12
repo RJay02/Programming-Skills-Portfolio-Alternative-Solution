bf3927f52a5e6a6d13679f5a8f41838c0fbc82b2
RJay02 <115995680+RJay02@users.noreply.github.com>
2024-02-12 18:58:19 +0000

    Creating the animal class with the data members

--- Playing around in class/Playing around in class.cpp
+++ Playing around in class/Playing around in class.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string>
+#include <array>
+#include <vector>
 using namespace std; 
+
+//Creating the Animal class
+class Animal {
+	string Species; 
+	string Name;
+	string Colour; 
+	string Age; 
+	string Weight; 
+	string Noise; 
+}
+
+
+
 
 int main()
 {
