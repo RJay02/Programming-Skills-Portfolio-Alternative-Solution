9138f2bcea4f9dd843ab2b5fcd0f9584130bbc69
RJay02 <115995680+RJay02@users.noreply.github.com>
2024-02-13 02:47:46 +0000

    Adding functionality to the menu

--- Playing around in class/Playing around in class.cpp
+++ Playing around in class/Playing around in class.cpp
@@ -45,12 +45,6 @@ public:
 
 };
 
-int choice;
-	cout << "Main Menu" << endl; 
-	cout << "Pick an animal" << endl; 
-	cout << "Enter a number" << endl; 
-	cin >> choice; 
-
 
 int main(){
 	Animal animal;
@@ -63,5 +57,74 @@ int main(){
 	Cat.makeNoise(); 
 	Cat.eat("fish");
 
+
+	int choice;
+	cout << "Main Menu" << endl;
+	cout << "Pick an animal" << endl;
+	cout << "1. Cat" << endl; 
+	cout << "2. Dog" << endl; 
+	cout << "Enter a number to choose an animal: ";
+	cin >> choice;
+
+	if (choice == 1){
+		int choice2; 
+		cout << "Sub Menu" << endl; 
+		cout << "Pick an interaction" << endl; 
+		cout << "1. Say Hello" << endl; 
+		cout << "2. Make a noise" << endl; 
+		cout << "3. View Details" << endl;
+		cout << "Enter a number (1-3) to pick an interaction: "; 
+		cin >> choice2; 
+
+		if (choice2 == 1) {
+			Cat.sayHello(); 
+		}
+		else if (choice2 == 2) {
+			Cat.makeNoise(); 
+		}
+		else if (choice2 == 3) {
+			cout << "Details for" << Cat.Name << endl; 
+			cout << "Species: " << Cat.Species << endl; 
+			cout << "Colour: " << Cat.Colour <<endl; 
+			cout << "Age: " << Cat.Age <<endl; 
+			cout << "Weight: " << Cat.Weight <<endl; 
+			cout << "Noise: " << Cat.Noise <<endl; 
+		}
+		else {
+			cout << "Invalid choice. Please try again" << endl;
+		}
+
+	}
+	else if (choice == 2) {
+		int choice2;
+		cout << "Sub Menu" << endl;
+		cout << "Pick an interaction" << endl;
+		cout << "1. Say Hello" << endl;
+		cout << "2. Make a noise" << endl;
+		cout << "3. View Details" << endl;
+		cout << "Enter a number (1-3) to pick an interaction: ";
+		cin >> choice2;
+
+		if (choice2 == 1) {
+			Dog.sayHello();
+		}
+		else if (choice2 == 2) {
+			Dog.makeNoise();
+		}
+		else if (choice2 == 3) {
+			cout << "Details for" << Cat.Name << endl;
+			cout << "Species: " << Cat.Species << endl;
+			cout << "Colour: " << Cat.Colour << endl;
+			cout << "Age: " << Cat.Age << endl;
+			cout << "Weight: " << Cat.Weight << endl;
+			cout << "Noise: " << Cat.Noise << endl;
+		}
+		else {
+			cout << "Invalid choice. Please try again" << endl;
+		}
+	}
+	else {
+		cout << "Invalid choice. Please try again" << endl; 
+	}
 	return 0; 
 }
